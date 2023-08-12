91f18109069af29abacf0f7aeebad5baaaf5b736
woongdam777 <114451860+woongdam777@users.noreply.github.com>
2023-08-13 06:42:43 +0900

    Time: 1 ms (99.22%), Space: 44.2 MB (95.45%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,18 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
-        int count = 0;
+        int curmax = 0;
+        int nextmax = 0;
         int n=nums.length;
-        int max = 0;
-        int result = 0;
+        int jump = 0;
         for(int i = 0; i<n-1;i++){
-            if(i+nums[i]>max){
-                max=i+nums[i];
+            if(i+nums[i]>curmax){
+                curmax=i+nums[i];
             }
-            if(i==count){
-                result++;
-                count = max;
+            if(i==nextmax){
+                jump++;
+                nextmax = curmax;
             }
         }
-        return result;
+        return jump;
     }
 }
