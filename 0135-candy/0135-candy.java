764cb4a2a41b47c75fa273206e85c055bbd9b3c0
woongdam777 <114451860+woongdam777@users.noreply.github.com>
2023-08-19 21:19:49 +0900

    Time: 2 ms (99.90%), Space: 44.4 MB (17.36%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int candy(int[] ratings) {
+        if(ratings.length==1)
+            return 1;
+        int candies[]=new int[ratings.length];
+        for(int i=1;i<ratings.length;i++){
+            if(ratings[i]>ratings[i-1]&&candies[i]<=candies[i-1])candies[i]=candies[i-1]+1;
+        }
+        
+        for(int i=ratings.length-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]&&candies[i]<=candies[i+1])candies[i]=candies[i+1]+1;
+        }
+        
+        int sum=0;
+        for(int x:candies) sum+=x;
+        
+        return sum+ratings.length;
+    }
+}
